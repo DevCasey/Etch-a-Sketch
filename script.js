d16cd454cbca9ba61d39aa3e2da435b78af67fd5
Casey Calkins <c.angelcalkins@outlook.com>
2018-09-10 18:20:28 -0500

    JavaScript

--- script.js
+++ script.js
@@ -4,17 +4,20 @@ let newDiv = document.createElement('div');
 
 
 function createGrid(gridAmount) {
-	for (let i = 1; i <= gridAmount; i++) {
+	for (let i = 1; i < gridAmount; i++) {
 			const row = document.createElement('div');
-			row.classList.add('square')
 			container.appendChild(row);
-			for (let j = 0; j <= gridAmount; j++) {
+
+			for (let j = 0; j < gridAmount; j++) {
 				const square = document.createElement('div');
 				square.classList.add('square');
+				square.style.width = `${960 / gridAmount}px`;
+				square.style.height = `${500 / gridAmount}px`
 				row.appendChild(square);
 		}
 	}
 }
+
 
 function getSize() {
 	gridAmount = prompt("Enter a grid amount: ", 16);
