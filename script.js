74f85f0fb93afdc6cf0aee32c7dfdab5f7c5cedc
Casey Calkins <c.angelcalkins@outlook.com>
2018-09-12 16:24:12 -0500

    Updated code

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-let container = document.querySelector('.container');
-let id = document.querySelector('#id');
-let resetMe = document.querySelector('.reset');
+const container = document.querySelector('.container');
+const resetMe = document.querySelector('.reset');
+
+
 
 // Creates the grid
 function createGrid(gridAmount) {
 	for (let i = 0; i < gridAmount; i++) {
 			const row = document.createElement('div');
+			row.classList.add('outterDiv');
 			container.appendChild(row);
 
 			for (let j = 0; j < gridAmount; j++) {
@@ -18,29 +20,50 @@ function createGrid(gridAmount) {
 	}
 }
 
-
+// Creates a button element and assigns an ID of 'btn1'
 const btn1 = document.createElement('button');
+const btn2 = document.createElement('button');
 btn1.setAttribute('id', 'btn1');
+btn2.setAttribute('id', 'btn2');
+resetMe.appendChild(btn2);
 resetMe.appendChild(btn1);
 document.getElementById('btn1').textContent = "Reset me";
+document.getElementById('btn2').textContent = "Change Grid Size";
+
+btn2.addEventListener('click', changeGrid);
+function changeGrid() {
+	
 
 
-// Function that gets a random color on mouseover
+// A function that clears the screen when the Reset button is clicked
+btn1.addEventListener('click', resetGrid);
+function resetGrid(event) {
+	document.querySelectorAll('.square').forEach(square => {
+        const red = 255;
+	    const green = 255;
+	    const blue = 255;
+	    return square.style.backgroundColor = `rgb(${red},${green},${blue})`;
+	})
+}
 
 
-document.querySelector('.container').addEventListener('mouseover', getColor)
-
+// Function that changes the color of the grid on mouseover
+document.querySelector('.container').addEventListener('mouseover', getColor);
 function getColor(event) {
-	if (event.target.classList.contains('container')) {
-  	console.log('container');
+	if (event.target.classList.contains('container') || event.target.classList.contains('outterDiv')) {
+  	console.log('Not colored');
     return;
   }
+  /*const square = event.target;
+  const red = Math.floor((Math.random() * 256) + 1);
+  const green = Math.floor((Math.random() * 256) + 1);
+  const blue = Math.floor((Math.random() * 256) + 1);
+  return square.style.backgroundColor = `rgb(${red},${green},${blue})`;*/
   const square = event.target;
-	let red = Math.floor((Math.random() * 256) + 1);
-	let green = Math.floor((Math.random() * 256) + 1);
-	let blue = Math.floor((Math.random() * 256) + 1);
-	return square.style.backgroundColor = `rgb(${red},${green},${blue})`;
-	
+  const red = 0;
+  const green = 0;
+  const blue = 0;
+  return square.style.backgroundColor = `rgb(${red},${green},${blue})`;
 }
 
 
@@ -49,6 +72,7 @@ function getSize() {
 	gridAmount = prompt("Enter a grid amount: ", 16);
 	createGrid(gridAmount);
 }
+
 
 getSize();
 
